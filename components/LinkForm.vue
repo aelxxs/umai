aa8d05c026ae1bfb3e7f41b60df3c41bab9870c7
ohagiiman <alexis.vielma.us@gmail.com>
2020-08-26 16:24:27 -0500

    now

--- components/LinkForm.vue
+++ components/LinkForm.vue
@@ -42,7 +42,7 @@
         this.done = false;
 
         try {
-          const data = await this.$axios.$post("/v1/url", {
+          const data = await this.$axios.$post("https://api.umai.pw/v1/url", {
             url: this.url,
           });
 
