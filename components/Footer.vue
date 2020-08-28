c2b05052a06ba99f08277e51bc12e451e25e3a0a
ohagiiman <alexis.vielma.us@gmail.com>
2020-08-28 13:32:34 -0500

    footer links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,16 @@
 <template>
   <footer class="footer" style="user-select: none;">
-    <a href="/discord" class="link">
+    <a href="https://discord.gg/AuWCjAb" class="link">
       <fa-icon class="fa-lg" :icon="['fab','discord']" :style="{ color: '#7289DA' }" />
       <span class="text">Discord</span>
     </a>:
-    <a href="/github" class="link">
+    <a href="https://github.com/ohagiiman" class="link">
       <fa-icon class="fa-lg" :icon="['fab','github-alt']" :style="{ color: 'white' }" />
       <span class="text">Github</span>
     </a>:
     <a href="/" class="link">
-      <fa-icon class="fa-lg" icon="share" :style="{ color: '#ff304f' }" />
-      <span class="text">Soon</span>
+      <fa-icon class="fa-lg" icon="star" :style="{ color: '#ff304f' }" />
+      <span class="text">Dunno what to put here lol</span>
     </a>
   </footer>
 </template>
