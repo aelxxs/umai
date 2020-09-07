ca84a7a5c99e8e7acc6c7095cb918b4410106571
ohagiiman <alexis.vielma.us@gmail.com>
2020-09-07 09:47:10 -0500

    🥢 | I just want my commits to look cool -

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,8 +11,8 @@
     mounted() {
       console.log(
         `%c umai.pw %c v1.0.0 %c`,
-        "background: #191b25; padding: 1px; border-radius: 3px 0 0 3px;  color: #fff",
-        "background: #00cc74; padding: 1px; border-radius: 0 3px 3px 0; color: #fff",
+        "background: #191b25; padding: 1px; border-radius: 3px 0 0 3px;  color: #ffffff",
+        "background: #00cc74; padding: 1px; border-radius: 0 3px 3px 0; color: #ffffff",
         "background: transparent;"
       );
     },
